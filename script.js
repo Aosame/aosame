09bcd6331d73b60164becef55b696d19dd05d9ca
AOSAME <aosame.since2025@gmail.com>
2025-07-03 11:17:27 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// script.js
+document.addEventListener('DOMContentLoaded', function () {
+  const images = document.querySelectorAll('.hero img');
+  let current = 0;
+
+  function changeImage() {
+    images[current].classList.remove('current');
+    current = (current + 1) % images.length;
+    images[current].classList.add('current');
+  }
+
+  setInterval(changeImage, 4000);
+});
