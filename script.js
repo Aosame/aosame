c07d5e5b43942f37fa758f10accd93a260679cc4
AOSAME <aosame.since2025@gmail.com>
2025-07-03 17:13:15 +0900

    Update script.js

--- script.js
+++ script.js
@@ -11,3 +11,22 @@ document.addEventListener('DOMContentLoaded', function () {
 
   setInterval(changeImage, 4000);
 });
+
+
+
+  window.addEventListener('load', () => {
+    // 各文字にランダムな方向を設定
+    document.querySelectorAll('.letter').forEach(letter => {
+      letter.style.setProperty('--rand-x', Math.random());
+      letter.style.setProperty('--rand-y', Math.random());
+    });
+
+    // 3秒後にコンテンツ表示
+    setTimeout(() => {
+      document.getElementById('splash').style.display = 'none';
+      document.getElementById('main-content').style.display = 'block';
+    }, 3000);// 3秒後にコンテンツ表示
+  });
+
+
+
